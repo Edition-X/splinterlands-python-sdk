2a93b77b68387bbf98255a0a507a8f63d20804cb
Dan Kelly <daniel.kelly1@ocado.com>
2022-12-24 11:49:02 +0100

    Add type strictness to the api

--- SplinterlandsAPI.py
+++ SplinterlandsAPI.py
@@ -4,20 +4,18 @@ import time
 import json
 
 class SplinterlandsAPI:
-    def __init__(self):
-        self.base_url = "https://api.splinterlands.com"
-        self.cache = {}
+    def __init__(self) -> None:
+        self.base_url: str = "https://api.splinterlands.com"
+        self.cache: dict = {}
 
 
-    def _get_headers(self):
+    def _get_headers(self) -> dict:
         return {
         }
 
-    def _make_request(self, endpoint, params=None):
-        if not params:
-            params = {}
+    def _make_request(self, endpoint: str, params: dict = {}) -> dict:
         # Check if the data is already in the cache
-        cache_key = f"{endpoint}:{json.dumps(params)}"
+        cache_key: str = f"{endpoint}:{json.dumps(params)}"
         if cache_key in self.cache:
             cached_data, cache_timestamp = self.cache[cache_key]
             age = time.time() - cache_timestamp
@@ -25,24 +23,24 @@ class SplinterlandsAPI:
                 return cached_data
 
         # If the data is not in the cache, make the API request
-        url = f"{self.base_url}/{endpoint}"
-        response = requests.get(url, params=params, headers=self._get_headers())
+        url: str = f"{self.base_url}/{endpoint}"
+        response: requests.Response = requests.get(url, params=params, headers=self._get_headers())
         if response.status_code != 200:
             raise Exception(f"Request to {url} failed with status code {response.status_code}")
-        data = response.json()
+        data: dict = response.json()
 
         # Save the data in the cache
         self.cache[cache_key] = (data, time.time())
         return data
 
-    def get_cards(self):
-        endpoint = "cards/get_details"
+    def get_cards(self) -> dict:
+        endpoint: str = "cards/get_details"
         return self._make_request(endpoint)
 
-    def get_settings(self):
-        endpoint = "settings"
+    def get_settings(self) -> dict:
+        endpoint: str = "settings"
         return self._make_request(endpoint)
 
-    def get_specific_cards(self, uid):
-        endpoint = f"cards/find?ids={uid}"
+    def get_specific_cards(self, uid: str) -> dict:
+        endpoint: str = f"cards/find?ids={uid}"
         return self._make_request(endpoint)
