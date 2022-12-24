485a876620e6e849dbe4d55c13a0309364e83024
Dan Kelly <daniel.kelly1@ocado.com>
2022-12-24 10:46:29 +0100

    Add test for get_cards function

--- tests/test_api.py
+++ tests/test_api.py
@@ -9,25 +9,35 @@ class TestSplinterlandsAPI(unittest.TestCase):
     def setUp(self):
         self.api = SplinterlandsAPI()
 
+    def test_get_cards(self):
+        # Make the API call
+        cards = self.api.get_cards()
+        # Verify that the response is a list
+        self.assertIsInstance(cards, list)
+        # Verify that the list is not empty
+        self.assertTrue(cards)
+        # Verify that each element in the list is a dictionary
+        for card in cards:
+            self.assertIsInstance(card, dict)
+        # Verify that each card has an 'id' field
+        for card in cards:
+            self.assertIn('id', card)
+
     def test_caching(self):
         # First, clear the cache
         self.api.cache = {}
-
         # Make the first API call
         start_time = time.time()
         cards1 = self.api.get_cards()
         end_time = time.time()
         elapsed_time1 = end_time - start_time
-
         # Make the second API call
         start_time = time.time()
         cards2 = self.api.get_cards()
         end_time = time.time()
         elapsed_time2 = end_time - start_time
-
         # Verify that the data returned by the second call is the same as the first
         self.assertEqual(cards1, cards2)
-
         # Verify that the second call was much faster than the first, indicating that it was served from the cache
         self.assertLess(elapsed_time2, elapsed_time1 / 10)
 
