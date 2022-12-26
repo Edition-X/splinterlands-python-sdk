3f6e83fe8f73229e6947fef274562384fe808049
Dan Kelly <daniel.kelly1@ocado.com>
2022-12-26 11:57:49 +0100

    Remove blockchain and move account parameter

--- SplinterlandsSDK/Hive.py
+++ SplinterlandsSDK/Hive.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
-from beem.blockchain import Blockchain
 from beem import Hive
 class Hive:
     def __init__(self, hive_active_key) -> None:
         self.hive = Hive(keys=hive_active_key)
-        self.blockchain = Blockchain(blockchain_instance=self.hive, mode="head")
 
-    def custom_json(self, account, message, json):
+    def custom_json(self, message, json, account):
         try:
             self.hive.custom_json(message, json_data=json, required_auths=[account])
         except Exception as e:
