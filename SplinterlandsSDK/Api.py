f3fe496178682e4d99270a8855c05c4045be59c8
Dan Kelly <daniel.kelly1@ocado.com>
2022-12-28 14:24:53 +0100

    Update caching for just get_cards

--- SplinterlandsSDK/Api.py
+++ SplinterlandsSDK/Api.py
@@ -15,15 +15,6 @@ class Api:
         }
 
     def _make_request(self, endpoint: str, params: dict = {}) -> Union[dict, List]:
-        # Check if the data is already in the cache
-        cache_key: str = f"{endpoint}:{json.dumps(params)}"
-        # if  endpoint != "market/for_sale_grouped" and cache_key in self.cache:
-        if  cache_key in self.cache:
-            cached_data, cache_timestamp = self.cache[cache_key]
-            age = time.time() - cache_timestamp
-            if age < 300:  # 5 minutes
-                return cached_data
-
         # If the data is not in the cache, make the API request
         url: str = f"{self.base_url}/{endpoint}"
         response: requests.Response = requests.get(url, params=params, headers=self._get_headers())
@@ -31,13 +22,22 @@ class Api:
             raise Exception(f"Request to {url} failed with status code {response.status_code}")
         data: dict = response.json()
 
-        # Save the data in the cache
-        self.cache[cache_key] = (data, time.time())
         return data
 
     def get_cards(self) -> Union[dict, List]:
         endpoint: str = "cards/get_details"
-        return self._make_request(endpoint)
+        # Check if the data is already in the cache
+        cache_key: str = f"{endpoint}"
+        if  cache_key in self.cache:
+            cached_data, cache_timestamp = self.cache[cache_key]
+            age = time.time() - cache_timestamp
+            if age < (30 * 24 * 60 * 60):  # 5 minutes
+                return cached_data
+
+        data = self._make_request(endpoint)
+        # Save the data in the cache
+        self.cache[cache_key] = (data, time.time())
+        return data
 
     def get_settings(self) -> Union[dict, List]:
         endpoint: str = "settings"
